3c661263c1448d203ac4bcad8ba6041e40a6d6f6
Elizabeth Langenstein <elizabeth.langenstein@kellogg.northwestern.edu>
2024-05-29 20:02:14 +0000

    work on practice exercise

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,27 @@ class SessionsController < ApplicationController
   
   def create
     # TODO: authenticate user
-    flash["notice"] = "Nope."
-    redirect_to "/login"
-  end
+    @user = User.find_by({"email"=>params["email"]})
+
+    if @user 
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome"
+        redirect_to "/posts"
+      else 
+        flash["notice"] = "Nope."
+        redirect_to "/login"
+      end 
+    else 
+      flash["notice"] = "Nope."
+      redirect_to "/login"
+    end
 end
+
+def destroy
+  session["user_id"]=nil
+  flash["notice"]="Goodbye"
+  redirect_to "/login"
+end 
+end
+
